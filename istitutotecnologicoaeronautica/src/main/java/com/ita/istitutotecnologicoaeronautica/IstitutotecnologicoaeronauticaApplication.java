c73df87abf1364c866be8a5c8885758fdd73f64c
itaBraatz <itabraatz@gmail.com>
2026-09-17 10:13:25 -0300

    Add files via upload

---
+++ istitutotecnologicoaeronautica/src/main/java/com/ita/istitutotecnologicoaeronautica/IstitutotecnologicoaeronauticaApplication.java
@@ -0,0 +1,13 @@
+package com.ita.istitutotecnologicoaeronautica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstitutotecnologicoaeronauticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IstitutotecnologicoaeronauticaApplication.class, args);
+	}
+
+}
