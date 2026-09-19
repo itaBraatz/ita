c311074add41bc5f2c6f978cdebf655474c36f62
Braatz <itabraatz@gmail.com>
2026-09-19 15:14:22 -0300

    ajuste

--- istitutotecnologicoaeronautica/src/main/java/com/ita/istitutotecnologicoaeronautica/LoginController.java
+++ istitutotecnologicoaeronautica/src/main/java/com/ita/istitutotecnologicoaeronautica/LoginController.java
@@ -17,15 +17,37 @@ public class LoginController {
     @PostMapping("/login")
     public String login(
         @RequestParam("username") String username,
-        @RequestParam("password") String password) {
+        @RequestParam("password") String password,
+        @RequestParam("year") String year,
+        @RequestParam("matter") String matter) {
 
-    System.out.println("Username: " + username);
-    System.out.println("Password: " + password);
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+        System.out.println("Year: " + year);
+        System.out.println("Matter: " + matter);
 
     if (username.equals("admin") && password.equals("123")) {
-        return "inicio";
+
+        switch (matter) {
+
+            case "Mathematics":
+
+                if (year.equals("2026")) {
+                    return "mathematics2026";
+                }
+
+                break;
+
+            case "Physics":
+                
+                break;
+
+            case "Chemistry":
+
+                break;
+        }
     }
 
     return "login";
-}
+    }
 }
